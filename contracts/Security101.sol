e4a4102d763a663fef527dacbcb9965c4d0c54d1
Bogdan Marin <bogdan.marin@softbinator.com>
2023-05-09 18:07:01 +0300

    format

--- contracts/Security101.sol
+++ contracts/Security101.sol
@@ -1,19 +1,20 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity 0.8.15;
+// // SPDX-License-Identifier: AGPL-3.0
+// pragma solidity 0.8.15;
 
-contract Security101 {
-    mapping(address => uint256) balances;
+// contract Security101 {
+//     mapping(address => uint256) balances;
 
-    function deposit() external payable {
-        balances[msg.sender] += msg.value;
-    }
+//     function deposit() external payable {
+//         balances[msg.sender] += msg.value;
+//     }
 
-    function withdraw(uint256 amount) external {
-        require(balances[msg.sender] >= amount, 'insufficient funds');
-        (bool ok, ) = msg.sender.call{value: amount}('');
-        require(ok, 'transfer failed');
-        unchecked {
-            balances[msg.sender] -= amount;
-        }
-    }
-}
+//     function withdraw(uint256 amount) external {
+//         require(balances[msg.sender] >= amount, 'insufficient funds');
+//         (bool ok, ) = msg.sender.call{value: amount}('');
+//         require(ok, 'transfer failed');
+//         unchecked {
+//             balances[msg.sender] -= amount;
+//         }
+//     }
+// }
+
